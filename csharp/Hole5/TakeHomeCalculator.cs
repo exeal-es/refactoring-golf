18c8dafb47cd3d4953f1ee64d915327ec74ccb2c
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-04 12:09:52 +0100

    Refactor: H5->H6 TaxRate moved to its own file

--- csharp/Hole5/TakeHomeCalculator.cs
+++ csharp/Hole5/TakeHomeCalculator.cs
@@ -1,30 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Hole5
 {
-    public class TaxRate
-    {
-        private readonly int _percent;
-
-        private TaxRate(int percent)
-        {
-            _percent = percent;
-        }
-
-        public static TaxRate Of(int percent)
-        {
-            return new TaxRate(percent);
-        }
-
-        public Money Apply(Money first, Money total)
-        {
-            Double amount = total.value * (_percent / 100d);
-            return Money.Create(Convert.ToInt32(amount), first.currency);
-        }
-    }
-
     public class TakeHomeCalculator
     {
         private readonly TaxRate taxRate;
---
+++ csharp/Hole5/TaxRate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hole5
+{
+    public class TaxRate
+    {
+        private readonly int _percent;
+
+        private TaxRate(int percent)
+        {
+            _percent = percent;
+        }
+
+        public static TaxRate Of(int percent)
+        {
+            return new TaxRate(percent);
+        }
+
+        public Money Apply(Money first, Money total)
+        {
+            Double amount = total.value * (_percent / 100d);
+            return Money.Create(Convert.ToInt32(amount), first.currency);
+        }
+    }
+}
